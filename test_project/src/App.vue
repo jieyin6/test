8d4210ab3006540566ad75c6e26911f4e48802fe
jieyin6 <961145934@qq.com>
2018-01-16 21:01:12 +0800

    secondAdd_test

---
+++ test_project/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <input-number v-model="value" :max="10" :min="0"></input-number>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import a from "./components/input-number"
+export default {
+  name: 'App',
+  components:{
+    "input-number":a
+  },
+  data:function(){
+    return {
+      value:5
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
